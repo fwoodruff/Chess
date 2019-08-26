8baa6a7550c9ddd5502a3028814e5eadbad5a971
Freddie Woodruff <fwoodruff2@gmail.com>
2019-08-26 22:34:10 +0100

    Fixed TODO comment

--- Chess/Types/PiecesAndSquares.hpp
+++ Chess/Types/PiecesAndSquares.hpp
@@ -45,7 +45,7 @@ namespace chs {
         a2, b2, c2, d2, e2, f2, g2, h2,
         a1, b1, c1, d1, e1, f1, g1, h1,
     };
-    // replace this with an e_boardSquare::operator[] overload
+    // see stackoverflow question 21295935, and rename e_boardSquare to Tile
     constexpr e_boardSquare c_allBoardSquares[] = {
         a8, b8, c8, d8, e8, f8, g8, h8,
         a7, b7, c7, d7, e7, f7, g7, h7,

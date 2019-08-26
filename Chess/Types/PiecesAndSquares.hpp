693796701ac48a2a19f38ee56db32e9eda2e34ac
Freddie Woodruff <fwoodruff2@gmail.com>
2019-08-26 22:26:27 +0100

    Todo comment added

--- Chess/Types/PiecesAndSquares.hpp
+++ Chess/Types/PiecesAndSquares.hpp
@@ -45,6 +45,7 @@ namespace chs {
         a2, b2, c2, d2, e2, f2, g2, h2,
         a1, b1, c1, d1, e1, f1, g1, h1,
     };
+    // replace this with an e_boardSquare::operator[] overload
     constexpr e_boardSquare c_allBoardSquares[] = {
         a8, b8, c8, d8, e8, f8, g8, h8,
         a7, b7, c7, d7, e7, f7, g7, h7,
